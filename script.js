415214292001ae10396ba9da496017803ebfe98e
Christopher Weddington <cwed777@gmail.com>
2022-03-15 01:40:31 -0500

    initial calendar attempt

---
+++ script.js
@@ -0,0 +1,10 @@
+// create a 24hr per day calendar
+// begin by creating a 31 day calendar
+const calendar = document.querySelector("#app-calendar");
+console.log(calendar)
+
+let day 
+
+for (let day = 1; day <= 31; day++); {
+    console.log(day);
+}
